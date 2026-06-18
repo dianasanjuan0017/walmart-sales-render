6f3a7e4b8733106bb63078fd2e3f91689b204c3f
novagrafoficial-crypto <novagraf.oficial@gmail.com>
2026-06-18 01:55:46 -0600

    Mejora interfaz Walmart

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pandas as pd
 import joblib
-
 
 app = Flask(__name__)
 
@@ -10,40 +9,40 @@ modelo = joblib.load("walmart_model.pkl")
 
 @app.route("/")
 def home():
-
     return render_template("formulario.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
 
-
     datos = request.form
 
+    # Convertir fecha
+    fecha = pd.to_datetime(datos["Date"])
+
+    year = fecha.year
+    month = fecha.month
+    day = fecha.day
+    week_of_year = fecha.isocalendar().week
 
     entrada = pd.DataFrame([{
-
         "Store": int(datos["Store"]),
         "Temperature": float(datos["Temperature"]),
         "Fuel_Price": float(datos["Fuel_Price"]),
         "CPI": float(datos["CPI"]),
         "Unemployment": float(datos["Unemployment"]),
-        "Year": int(datos["Year"]),
-        "Month": int(datos["Month"]),
-        "Day": int(datos["Day"]),
-        "Week_of_Year": int(datos["Week_of_Year"])
-
+        "Year": year,
+        "Month": month,
+        "Day": day,
+        "Week_of_Year": week_of_year
     }])
-
 
     prediccion = modelo.predict(entrada)
 
-
     return render_template(
         "formulario.html",
-        prediction=round(float(prediccion[0]),2)
+        prediction=round(float(prediccion[0]), 2)
     )
-
 
 
 if __name__ == "__main__":
